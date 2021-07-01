53bd7a611c9c6a9b355fcacd7479ea8bd32c8841
Yujindawang <yujinhao1996@sjtu.edu.cn>
2021-07-01 14:51:43 +0800

    code

--- DAL/hyp.py
+++ DAL/hyp.py
@@ -1,14 +1,3 @@
-# lr
-lr0: 0.0001
-warmup_lr: 0.00001   
-warm_epoch:5
-
-
-# setting
-num_classes: 1 #10
-
-# training
-epochs: 300
-batch_size: 2
-save_interval: 5
-test_interval: 5
+version https://git-lfs.github.com/spec/v1
+oid sha256:166472a0778bc1a908470e9b3e0c18db7e4f77f3c533d91ea1fbfccbab46f2c7
+size 155
